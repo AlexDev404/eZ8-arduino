7d283cfe49b755439511822d38935b7645b23d7c
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-05-27 03:44:37 -0600

    Working page unlock code

--- src/flash_tools.c
+++ src/flash_tools.c
@@ -17,27 +17,31 @@ unsigned char checkFlash(void) {
 
 int pageEraseFlash(unsigned long address) {
 	unsigned char pageNum;
+	int i = 0;
 	
 	if (FCMD == 0x03) return -1;
 	
 	// Calculate page number from address (512-byte pages)
 	pageNum = (unsigned char)((address >> 9) & 0xFF);
 	
-	// Set page number for erase
-	PAGE = pageNum;
-
-	// Unlock Flash
-	unlockFlash();
-	
-	// Set page number for erase
-	PAGE = pageNum;
-	
-	// Erase
-	FCMD = 0x95;
-	
-	///////////////////////
-	
+	// According to the flow diagram in PS0228 and AN0328
+	for (i = 0; i < 1; i++) {
 		// Set page number for erase
+		PAGE = pageNum;
+		// Unlock Flash
+		unlockFlash();
+		// Set page number for erase
+		PAGE = pageNum;
+		// Erase
+		FCMD = 0x95;
+	}
+	
+	// Wait for erase to complete (status returns to 0)
+	while (FCMD != 0x00);	
+	
+	///////// UNLOCK THE PAGE ///////////
+	
+	// Set page number for erase
 	PAGE = pageNum;
 
 	// Unlock Flash
@@ -45,11 +49,12 @@ int pageEraseFlash(unsigned long address) {
 	
 	// Set page number for erase
 	PAGE = pageNum;
+	
 	
     // Todo: Optionally, verify erased (all 0xFF) here
 	//if (FCMD == 0x00) return -1;
-	// Wait for erase to complete (status returns to 0)
-	//while (FCMD != 0x00);
+	// Wait for unlock to complete (status returns to 3)
+	while (FCMD != 0x03);
 	return 0; // Success
 }
 
